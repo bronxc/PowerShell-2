24f6e31acb206e8569ca4894e55fce41dfec34f6
muhammad-majid <42956457+muhammad-majid@users.noreply.github.com>
2019-11-09 13:27:44 +1000

    Update rebootAfterNotification.cmd

--- rebootAfterNotification.cmd
+++ rebootAfterNotification.cmd
@@ -3,7 +3,7 @@ REM Notify users that we are rebooting server
 msg * This server will be rebooted in 15 minutes. Please save your work and log out until 2:30AM.
 REM Disable new user logins to this machine
 REM change logon /disable
-sleep 600
+timeout /t 600 /nobreak > NUL
 msg * This server will be rebooted in 5 minutes. Please save your work and log out until 2:30AM.
-sleep 300
+timeout /t 300 /nobreak > NUL
 shutdown /r /f /t 0
